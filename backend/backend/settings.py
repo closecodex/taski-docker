807197655fefd3719e449c21e3e58119ee1b9e06
closecodex <closecodex@gmail.com>
2024-10-04 18:33:22 +0100

    final

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '89.169.173.241', 'practicumhost.hopto.org']
+ALLOWED_HOSTS = ['localhost', '89.169.173.241', 'yapracticumhost.hopto.org']
 
 
 # Application definition
